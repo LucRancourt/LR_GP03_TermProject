db5b79d0135ad1c78d0006a620e81f44e248f4e1
LucRancourt <welpidkwhat@gmail.com>
2025-09-17 05:06:28 -0400

    Implemented Player Movement

---
+++ TermProject_03/Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    // Variables
+        private InputControls _inputControls;
+
+        #region Actions
+            public event Action<Vector2> MoveEvent;
+
+            // Camera Movement Events
+            public event Action<Vector2> LookEvent;
+            public event Action<bool> RotateCamEvent;
+            public event Action<Vector2> ZoomEvent;
+        #endregion
+
+
+    // Functions
+        private void Awake()
+        {
+            _inputControls = new InputControls();
+        }
+
+        private void OnEnable()
+        {
+            _inputControls.Player.Enable();
+
+            _inputControls.Player.Move.performed += OnMovePerformed;
+            _inputControls.Player.Move.canceled += OnMoveCanceled;
+        
+            // Camera Movement Events
+            _inputControls.Player.Look.performed += OnLookPerformed;
+            _inputControls.Player.RotateCam.performed += OnRotateCamPerformed;
+            _inputControls.Player.Zoom.performed += OnZoomPerformed;
+        }
+
+        #region Handlers
+            private void OnMovePerformed(InputAction.CallbackContext context)
+            {
+                MoveEvent?.Invoke(context.ReadValue<Vector2>());
+            }
+
+            private void OnMoveCanceled(InputAction.CallbackContext context)
+            {
+                MoveEvent?.Invoke(Vector2.zero);
+            }
+
+            private void OnLookPerformed(InputAction.CallbackContext context)
+            {
+                LookEvent?.Invoke(context.ReadValue<Vector2>());
+            }
+
+            private void OnRotateCamPerformed(InputAction.CallbackContext context)
+            {
+                RotateCamEvent?.Invoke(context.ReadValueAsButton());
+            }
+
+            private void OnZoomPerformed(InputAction.CallbackContext context)
+            {
+                ZoomEvent?.Invoke(context.ReadValue<Vector2>());
+            }
+        #endregion
+
+        private void OnDisable()
+        {
+            _inputControls.Player.Move.performed -= OnMovePerformed;
+            _inputControls.Player.Move.canceled -= OnMoveCanceled;
+        
+            // Camera Movement Events
+            _inputControls.Player.Look.performed -= OnLookPerformed;
+            _inputControls.Player.RotateCam.performed -= OnRotateCamPerformed;
+            _inputControls.Player.Zoom.performed -= OnZoomPerformed;
+
+            _inputControls.Player.Disable();
+        }
+}
---
+++ TermProject_03/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,164 @@
+using UnityEngine;
+
+[RequireComponent(typeof(InputController))]
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+        private InputController _inputController;
+        private CharacterController _characterController;
+
+        #region Move Vars
+            private Vector2 _moveInput;
+
+            private Vector3 _moveDirection;
+
+            private Vector3 _moveVelocity;
+            private Vector3 _targetMoveVelocity;
+
+            [Header("Movement")]
+            [SerializeField, Min(0.0f)] private float targetMoveSpeed = 12.0f;
+        #endregion
+
+        #region Look Vars
+            private Vector2 _lookInput;
+            private bool _isRotatingCam = false;
+            private Vector2 _zoomInput;
+
+            private Camera _mainCamera;
+
+            private Vector2 _currentMouseDelta;
+            private Vector2 _currentMouseVelocity;
+
+            private float _lookTargetRotX;
+
+            private Vector3 _targetZoom;
+            private Vector3 _currentZoomVelocity;
+
+            [Header("Camera Rotation")]
+            [SerializeField, Min(0.01f)] private float lookSpeed = 0.25f;
+            [SerializeField, Min(0.01f)] private float lookSmoothTime = 0.05f;
+
+            [SerializeField, Min(0.0f)] private float xCameraBounds = 80.0f;
+    
+            [Header("Camera Zoom")]
+            [SerializeField, Min(0.01f)] private float zoomSpeed = 0.5f;
+            [SerializeField, Min(0.01f)] private float zoomSmoothTime = 0.05f;
+            [SerializeField, Min(0.0f)] private float zoomStrength = 100.0f;
+        #endregion
+
+        [Header("Interactable")]
+        [SerializeField] private LayerMask interactLayer;
+
+
+    // Functions
+        private void Awake()
+        {
+            _inputController = GetComponent<InputController>();
+
+            _characterController = GetComponent<CharacterController>();
+            _characterController.detectCollisions = false;
+
+            _mainCamera = GetComponentInChildren<Camera>();
+
+            if (_mainCamera == null)
+                Debug.LogError("MainCamera is not a Child of the Player!");
+
+            Cursor.visible = true;
+        }
+
+        #region InputController - OnEnable/OnDisable
+            private void OnEnable()
+            {
+                if (_inputController != null)
+                {
+                    _inputController.MoveEvent += HandleMoveInput;
+
+                    _inputController.LookEvent += HandleLookInput;
+                    _inputController.RotateCamEvent += HandleRotateCamInput;
+                    _inputController.ZoomEvent += HandleZoomInput;
+                }
+            }
+
+            private void OnDisable()
+            {
+                if (_inputController != null)
+                {
+                    _inputController.MoveEvent -= HandleMoveInput;
+
+                    _inputController.LookEvent -= HandleLookInput;
+                    _inputController.RotateCamEvent -= HandleRotateCamInput;
+                    _inputController.ZoomEvent -= HandleZoomInput;
+                }
+            }
+        #endregion
+
+        #region HandleInputs
+            private void HandleMoveInput(Vector2 movement)
+            {
+                _moveInput = movement;
+            }
+
+            private void HandleLookInput(Vector2 look)
+            {
+                _lookInput = look;
+            }
+
+            private void HandleRotateCamInput(bool rotateCam)
+            {
+                _isRotatingCam = rotateCam;
+
+                if (_isRotatingCam)
+                    Cursor.lockState = CursorLockMode.Locked;
+                else
+                    Cursor.lockState = CursorLockMode.None;
+            }
+
+            private void HandleZoomInput(Vector2 zoom)
+            {
+                _zoomInput = zoom.normalized;
+            }
+        #endregion
+
+
+        private void Update()
+        {
+            Move();
+            Look();
+            Zoom();
+
+            _moveVelocity = _targetMoveVelocity + _targetZoom;
+            _characterController.Move(_moveVelocity * Time.deltaTime);
+        }
+
+        private void Move()
+        {
+            _moveDirection = transform.forward * _moveInput.y + transform.right * _moveInput.x;
+            _moveDirection.Normalize();
+
+            _targetMoveVelocity = _moveDirection * targetMoveSpeed;
+        }
+
+        private void Look()
+        {
+            if (!_isRotatingCam) return;
+
+
+            Vector2 targetDelta = _lookInput * lookSpeed;
+
+            _currentMouseDelta = Vector2.SmoothDamp(_currentMouseDelta, targetDelta, ref _currentMouseVelocity, lookSmoothTime);
+
+            _lookTargetRotX = MyUtils.Clamp(_lookTargetRotX - _currentMouseDelta.y, -xCameraBounds, xCameraBounds);
+
+            transform.rotation = Quaternion.Euler(0.0f, transform.rotation.eulerAngles.y + _currentMouseDelta.x, 0.0f);
+            _mainCamera.transform.localRotation = Quaternion.Euler(_lookTargetRotX, 0.0f, 0.0f);
+        }
+
+        private void Zoom()
+        {
+            Vector3 targetZoom = _mainCamera.transform.forward * _zoomInput.y * zoomStrength * zoomSpeed;
+            
+            _targetZoom = Vector3.SmoothDamp(_targetZoom, targetZoom, ref _currentZoomVelocity, zoomSmoothTime);
+        }
+}
