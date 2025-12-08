9c800b56157f234f9eeaf0522ff66b8dae571368
LucRancourt <welpidkwhat@gmail.com>
2025-12-07 20:05:06 -0500

    Adjusted Healthbar and made the Base one dynamically change color

--- TermProject_03/Assets/_Project/Code/Gameplay/HP/HealthView.cs
+++ TermProject_03/Assets/_Project/Code/Gameplay/HP/HealthView.cs
@@ -5,32 +5,53 @@ using UnityEngine.UI;
 
 public class HealthView : BaseView<float>
 {
-    [SerializeField] private Slider healthBar;
+    [SerializeField] private Image healthBar;
     [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private bool bDynamicColor = true;
+
+    private Color _red = new Color(227.0f / 255.0f, 61.0f / 255.0f, 65.0f / 255.0f);//new Color(254.0f / 255.0f, 161.0f / 255.0f, 163.0f / 255.0f);
+    private Color _yellow = new Color(235.0f / 237.0f, 227.0f / 255.0f, 0.0f / 255.0f);//new Color(254.0f / 255.0f, 251.0f / 255.0f, 161.0f / 255.0f);
+    private Color _green = new Color(69.0f / 255.0f, 227.0f / 255.0f, 61.0f / 255.0f);//new Color(161.0f / 255.0f, 254.0f / 255.0f, 167.0f / 255.0f);
 
 
     public override void Initialize()
     {
         base.Initialize();
 
-        if (healthBar != null)
+        if (healthBar != null && healthBar.type == Image.Type.Filled)
         {
-            healthBar.interactable = false;
-            healthBar.value = 1.0f;
+            healthBar.fillAmount = 1.0f;
+
+            if (bDynamicColor)
+                UpdateColor(1.0f);
         }
 
         if (healthText != null)
-        {
             healthText.text = "#/0";
-        }
     }
 
     public override void UpdateDisplay(float currentHealth, float maxHealth)
     {
-        if (healthBar != null)
-            healthBar.value = currentHealth / maxHealth;
+        if (healthBar != null && healthBar.type == Image.Type.Filled)
+        {
+            float currentPercentage = currentHealth / maxHealth;
+            healthBar.fillAmount = currentPercentage;
+
+            if (bDynamicColor)
+                UpdateColor(currentPercentage);
+        }
 
         if (healthText != null)
             healthText.text = currentHealth + "/" + maxHealth;
+    }
+
+    private void UpdateColor(float currentPercentage)
+    {
+        if (currentPercentage <= 0.33f)
+            healthBar.color = _red;
+        else if (currentPercentage <= 0.66f)
+            healthBar.color = _yellow;
+        else
+            healthBar.color = _green;
     }
 }
